dc4e3d5f0142673cadac4bcc1b4a17e9ae22d323
robertogomesbr <robertogomes0277@gmail.com>
2026-03-27 00:09:38 -0300

    Criando o projeto

---
+++ src/main/java/br/com/ifpe/gestacad/GestacadApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.gestacad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestacadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestacadApiApplication.class, args);
+	}
+
+}
