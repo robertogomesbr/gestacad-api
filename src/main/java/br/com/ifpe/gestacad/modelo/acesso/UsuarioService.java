f5ebbc4d01f9cc82c16ec03ea8ce2a9db559294c
rhuangomes10 <rhuancraft189gg@gmail.com>
2026-05-20 21:00:18 -0300

    Documentação no Swagger adicionada

--- src/main/java/br/com/ifpe/gestacad/modelo/acesso/UsuarioService.java
+++ src/main/java/br/com/ifpe/gestacad/modelo/acesso/UsuarioService.java
@@ -1,6 +1,5 @@
 package br.com.ifpe.gestacad.modelo.acesso;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
--- src/main/java/br/com/ifpe/gestacad/modelo/professor/Professor.java
+++ src/main/java/br/com/ifpe/gestacad/modelo/professor/Professor.java
@@ -44,4 +44,6 @@ public class Professor extends EntidadeAuditavel {
 
     @Column(nullable = false)
     private boolean ativo;
+
+    
 }
